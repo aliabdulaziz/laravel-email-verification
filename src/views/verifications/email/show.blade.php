6939caa9ac84843b6b06752a9ad496bceed2927e
Ali Abdulaziz <ali@aliabdulaziz.com>
2018-05-15 09:43:50 +0300

    correct a typo

--- src/views/verifications/email/show.blade.php
+++ src/views/verifications/email/show.blade.php
@@ -24,7 +24,7 @@
                         @if($status['type'] === 'danger')
 
                             @if($status['title'] === 'Email verification failed!')
-                            <p>Please try again, or click on the reset button to receive a new verification mail.</p>
+                            <p>Please try again, or click on the resend button to receive a new verification mail.</p>
                             @else
                             <p>We have sent the verification mail to <span class="text-primary">{{ $email }}</span>. If you cannot find the <i><u>email verification</u></i> mail in the Index folder, please check the Junk/Spam folder.</p>
                             <p>If you did not receive the <i><u>email verification</u></i> mail please click on the resend button.</p>
